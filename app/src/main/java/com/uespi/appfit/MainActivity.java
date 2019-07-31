c84ab27a5b40538c1228cac07b0ebcdbd48668d6
Diego Rodrigues <ddiegosr@hotmail.com>
2019-07-31 18:10:44 -0300

    added verifications in login screen

--- app/src/main/java/com/uespi/appfit/MainActivity.java
+++ app/src/main/java/com/uespi/appfit/MainActivity.java
@@ -2,13 +2,45 @@ package com.uespi.appfit;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity {
+    private EditText edtEmail;
+    private EditText edtPassword;
+    private Button btnLogin;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        edtEmail = findViewById(R.id.edtEmail);
+        edtPassword = findViewById(R.id.edtPassword);
+        btnLogin = findViewById(R.id.btnLogin);
+
+        btnLogin.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                String email = edtEmail.getText().toString();
+                String password = edtEmail.getText().toString();
+                if (email.isEmpty() || password.isEmpty()) {
+                    Toast.makeText(MainActivity.this, "Preenche todos os campos", Toast.LENGTH_SHORT).show();
+                    return;
+                }
+
+                if (!email.equals("admin@email.com") && !password.equals("adm123")) {
+                    Toast.makeText(MainActivity.this, "E-mail ou senha incorretos", Toast.LENGTH_SHORT).show();
+                    return;
+                }
+
+                Intent intent = new Intent(MainActivity.this, Home.class);
+                ((AppCompatActivity) MainActivity.this).startActivityForResult(intent, 1001);
+            }
+        });
     }
 }
