43a084c3921526c2238e645d05ba59a18d1e496b
Diego Rodrigues <ddiegosr@hotmail.com>
2019-08-01 21:32:30 -0300

    add click in recyclerview

--- app/src/main/java/com/uespi/appfit/adapters/InstructorAdapter.java
+++ app/src/main/java/com/uespi/appfit/adapters/InstructorAdapter.java
@@ -1,12 +1,15 @@
 package com.uespi.appfit.adapters;
 
+import android.content.Context;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.ImageView;
 import android.widget.TextView;
+import android.widget.Toast;
 
 import androidx.annotation.NonNull;
+import androidx.constraintlayout.widget.ConstraintLayout;
 import androidx.recyclerview.widget.RecyclerView;
 
 import com.uespi.appfit.R;
@@ -17,6 +20,7 @@ import java.util.List;
 public class InstructorAdapter extends RecyclerView.Adapter<InstructorAdapter.InstructorViewHolder> {
 
     private List<Instructor> instructors;
+    private Context context;
 
     public InstructorAdapter(List<Instructor> instructors) {
         this.instructors = instructors;
@@ -25,7 +29,8 @@ public class InstructorAdapter extends RecyclerView.Adapter<InstructorAdapter.In
     @NonNull
     @Override
     public InstructorViewHolder onCreateViewHolder(@NonNull ViewGroup parent, int viewType) {
-        View v = LayoutInflater.from(parent.getContext()).inflate(R.layout.instructor_item, parent, false);
+        context = parent.getContext();
+        View v = LayoutInflater.from(context).inflate(R.layout.instructor_item, parent, false);
         InstructorViewHolder ivh = new InstructorViewHolder(v);
         return ivh;
     }
@@ -42,16 +47,27 @@ public class InstructorAdapter extends RecyclerView.Adapter<InstructorAdapter.In
         return instructors.size();
     }
 
-    public static class InstructorViewHolder extends RecyclerView.ViewHolder {
+    public class InstructorViewHolder extends RecyclerView.ViewHolder {
         private TextView txtName;
         private TextView txtDescription;
         private ImageView imgPhoto;
+        private ConstraintLayout clInstructor;
 
         InstructorViewHolder(View itemView) {
             super(itemView);
             txtName = itemView.findViewById(R.id.txtName);
             txtDescription = itemView.findViewById(R.id.txtDescription);
             imgPhoto = itemView.findViewById(R.id.imgPhoto);
+            clInstructor = itemView.findViewById(R.id.clInstructor);
+
+            clInstructor.setOnClickListener(new View.OnClickListener() {
+                @Override
+                public void onClick(View view) {
+                    int itemIndex = getLayoutPosition();
+                    Instructor item = instructors.get(itemIndex);
+                    Toast.makeText(context, item.getName(), Toast.LENGTH_SHORT).show();
+                }
+            });
         }
     }
 }
